8d64e669579c2ebbf229606d3e4f3c9a5e9b977c
Hugo Roussel <hugo.roussel@univ-tlse3.fr>
2019-07-10 17:41:55 +0200

    Updated XmlConfigGenerator

--- AMOEBAonAMAK/src/experiments/nDimensionsLaunchers/F_N_Launcher.java
+++ AMOEBAonAMAK/src/experiments/nDimensionsLaunchers/F_N_Launcher.java
@@ -4,13 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 
-import agents.context.Context;
 import experiments.FILE;
-import experiments.F_XY_System;
-import experiments.XmlConfigGenerator;
 import fr.irit.smac.amak.Configuration;
 import gui.AmoebaWindow;
 import kernel.AMOEBA;
